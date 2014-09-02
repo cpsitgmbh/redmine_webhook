12918cc5476957fc18fd794f6ba006caf1cfb9ae
Nicole Cordes <cordes@cps-it.de>
2014-09-02 18:35:20 +0200

    [TASK] Plugin initialization

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_webhook do
+  name 'Redmine Webhook plugin'
+  author 'Nicole Cordes'
+  description 'This plugin registers Git repositories (e.g. coming from GitLab) in projects'
+  version '0.0.1'
+  url 'http://gitlab.cps-projects.de/gitlab/redmine_webhook'
+  author_url 'http://www.cps-it.de'
+end
